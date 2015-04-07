3280a7d35e1682e49168e2389f601decd4c8219a
Christoph Boehner <cb@vorwaerts-werbung.de>
2015-04-07 22:28:49 +0200

    Add stack content type

---
+++ ade25/assetmanager/browser/assetmanager.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""Module providing asset management views"""
+
+import json
+from Acquisition import aq_inner
+from Products.Five.browser import BrowserView
+
+
+class AssetRepositoryView(BrowserView):
+    """ Central management unit """
+
+    def __call__(self):
+        self.has_assets = len(self.assets()) > 0
+
+    def assets(self):
+        context = aq_inner(self.context)
+        data = getattr(context, 'assets')
+        if data is None:
+            data = dict()
+        return data
+
+    def stored_data(self):
+        return json.loads(self.assets())

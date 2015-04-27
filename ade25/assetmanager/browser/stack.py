0f93d6d61e1034357d06e016d3f77b6568f12bdf
Christoph Boehner <cb@vorwaerts-werbung.de>
2015-04-27 14:27:33 +0200

    Update asset manager templates
    Also fix missing `plone.api` dependency

--- ade25/assetmanager/browser/stack.py
+++ ade25/assetmanager/browser/stack.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """Module providing views for asset listings"""
+from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
 
 
 class StackView(BrowserView):
     """ Folderish stack content default view """
+
+    def addable_media_types(self):
+        context = aq_inner(self.context)
+        return context.getAllowedTypes()
--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'setuptools',
+        'plone.api',
         'plone.app.dexterity [grok, relations]',
         'plone.app.relationfield',
         'plone.namedfile [blobs]',

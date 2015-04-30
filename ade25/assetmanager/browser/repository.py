bd21cf6e3246636ff3482d23f13c859369b11b30
Christoph Boehner <cb@vorwaerts-werbung.de>
2015-04-30 23:10:01 +0200

    Update repository preview card layout
    refs #3

--- ade25/assetmanager/browser/repository.py
+++ ade25/assetmanager/browser/repository.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
 """Module providing views for asset storage folder"""
 from Products.Five.browser import BrowserView
+from plone import api
+from plone.app.blob.interfaces import IATBlobImage
 
 
 class AssetRepositoryView(BrowserView):
     """ Folderish content page default view """
+
+    def contained_items(self, uid):
+        stack = api.content.get(UID=uid)
+        return stack.restrictedTraverse('@@folderListing')()
+
+    def item_index(self, uid):
+        return len(self.contained_items(uid))
+
+    def preview_image(self, uid):
+        images = self.contained_items(uid)
+        preview = None
+        if len(images):
+            first_item = images[0].getObject()
+            if IATBlobImage.providedBy(first_item):
+                preview = first_item
+        return preview

3ab254cf429e4c0968b794b0ec496465f43e34a4
Christoph Boehner <cb@vorwaerts-werbung.de>
2015-04-30 23:34:01 +0200

    Port asset selection handler view
    refs #3

--- ade25/assetmanager/browser/assetmanager.py
+++ ade25/assetmanager/browser/assetmanager.py
@@ -2,6 +2,8 @@
 """Module providing asset management views"""
 
 import json
+import uuid as uuid_tool
+
 from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
 from plone import api
@@ -59,6 +61,69 @@ class SelectStack(BrowserView):
 class SelectAsset(BrowserView):
     """ Select assets from preselected stack """
 
+    def __call__(self):
+        self.has_assets = len(self.contained_assets()) > 0
+
+    @property
+    def traverse_subpath(self):
+        return self.subpath
+
+    def publishTraverse(self, request, name):
+        if not hasattr(self, 'subpath'):
+            self.subpath = []
+        self.subpath.append(name)
+        return self
+
+    def contained_assets(self):
+        uid = self.traverse_subpath[0]
+        stack = api.content.get(UID=uid)
+        images = stack.restrictedTraverse('@@folderListing')()
+        return images
+
 
 class AssignAsset(BrowserView):
     """ Assign asset to context specific asset storage """
+
+    def __call__(self):
+        return self.render()
+
+    @property
+    def traverse_subpath(self):
+        return self.subpath
+
+    def publishTraverse(self, request, name):
+        if not hasattr(self, 'subpath'):
+            self.subpath = []
+        self.subpath.append(name)
+        return self
+
+    def render(self):
+        context = aq_inner(self.context)
+        base_url = context.absolute_url()
+        stack = self.traverse_subpath[0]
+        next_url = '{0}/@@select-images/{1}'.format(base_url, stack)
+        self._add_item()
+        return self.request.response.redirect(next_url)
+
+    def assets(self):
+        context = aq_inner(self.context)
+        data = getattr(context, 'assets')
+        if data is None:
+            data = dict()
+        return data
+
+    def stored_data(self):
+        return json.loads(self.assets())
+
+    def _add_item(self):
+        data = self.stored_data()
+        uid = self.traverse_subpath[1]
+        items = data['items']
+        item = {
+            'id': uuid_tool.uuid4(),
+            'uid': uid,
+            'caption': ''
+        }
+        items.append(item)
+        data['items'] = items
+        return data

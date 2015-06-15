2897db35a0f126b5ef1732f5a2fdb1b921eb933f
Christoph Boehner <cb@vorwaerts-werbung.de>
2015-06-15 16:29:07 +0200

    Add stack preview

--- ade25/assetmanager/browser/stack.py
+++ ade25/assetmanager/browser/stack.py
@@ -10,3 +10,18 @@ class StackView(BrowserView):
     def addable_media_types(self):
         context = aq_inner(self.context)
         return context.getAllowedTypes()
+
+
+class StackPreview(BrowserView):
+    """ Reusable stack preview image """
+
+    def contained_items(self):
+        context = aq_inner(self.context)
+        items = context.restrictedTraverse('@@folderListing')()
+        return items
+
+    def has_contained_items(self):
+        return len(self.contained_items()) > 0
+
+    def first_item(self):
+        return self.contained_items()[0]
